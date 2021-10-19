d9b68ab8d0a4debd1da562f2b169667ba97e7542
Aron Vaupel <vaupelaron@gmail.com>
2021-10-19 14:24:46 +0200

    Create MediaQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import MediaQuery from 'react-responsive';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Disclaimer from './pages/Disclaimer/Disclaimer';
 import Home from './pages/Home/Home';
+import HomeDesktop from './pages/Home/HomeDesktop';
 import Location from './pages/Location/Location'
+
 
 
 
@@ -10,6 +13,18 @@ function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch> 
+       
+          <Route path="/disclaimer">
+          <Disclaimer />
+        </Route>
+        <Route path="/location">
+          <Location  />
+        </Route>
+        <Route path="/">
+          <HomeDesktop />
+        </Route>
+       
+        <MediaQuery query="(max-device-width: 768px)"> 
         <Route path="/disclaimer">
           <Disclaimer />
         </Route>
@@ -19,7 +34,7 @@ function App(): JSX.Element {
         <Route path="/">
           <Home />
         </Route>
-       
+       </MediaQuery>
       </Switch>
     </BrowserRouter>
   );
