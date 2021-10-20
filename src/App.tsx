79b070fdc43e4ad794481039da58643b1486bd7f
Aron Vaupel <vaupelaron@gmail.com>
2021-10-20 19:08:21 +0200

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,45 @@
-import React from 'react';
-import  { useMediaQuery } from 'react-responsive';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Disclaimer from './pages/Disclaimer/Disclaimer';
-import Home from './pages/Home/Home';
-import HomeDesktop from './pages/Home/HomeDesktop';
-import Location from './pages/Location/Location'
-
- 
-  
-
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Disclaimer from "./pages/Disclaimer/Disclaimer";
+import Home from "./pages/Home/Home";
+import HomeDesktop from "./pages/Home/HomeDesktop";
+import Location from "./pages/Location/Location";
 
 function App(): JSX.Element {
-const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
   return (
-   
     <BrowserRouter>
-      <Switch>  
-       {isMobile? 
-        
-        <><Route path="/disclaimer">
-            <Disclaimer />
-          </Route><Route path="/location">
+      <Switch>
+        {isMobile ? (
+          <>
+            <Route path="/disclaimer">
+              <Disclaimer />
+            </Route>
+            <Route path="/location">
               <Location />
-            </Route><Route path="/">
+            </Route>
+            <Route path="/">
               <Home />
-            </Route></>
-      :
-          <><Route path="/disclaimer">
-            <Disclaimer />
-          </Route><Route path="/location">
+            </Route>
+          </>
+        ) : (
+          <>
+            <Route path="/disclaimer">
+              <Disclaimer />
+            </Route>
+            <Route path="/location">
               <Location />
-            </Route><Route path="/">
+            </Route>
+            <Route path="/">
               <HomeDesktop />
-            </Route></>
-}
-      
+            </Route>
+          </>
+        )}
       </Switch>
     </BrowserRouter>
-  
-
-
   );
 }
 
 export default App;
-
