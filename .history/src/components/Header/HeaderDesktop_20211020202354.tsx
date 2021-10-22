ec12270860faeca27627d2a86b5914d6e31db62e
Aron Vaupel <vaupelaron@gmail.com>
2021-10-22 08:12:23 +0200

    format header

---
+++ .history/src/components/Header/HeaderDesktop_20211020202354.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './HeaderDesktop.module.css';
+
+
+
+
+const HeaderDesktop = (): JSX.Element => {
+
+
+
+  return (
+    <div className={styles.header}>
+      <Link to={'/'}>
+      <img src="/assets/logo.webp" alt="logo" />
+      
+      </Link> 
+      <h1 className={styles.headerAnimation}>WANGUI GATHERE <span>ADVOCATES</span></h1>
+      <div className={styles.links}>
+      <Link to={'/team'}> <h3>Our <span>TEAM</span></h3> </Link>
+      <Link to={'/services'}><h3>Our <span>SERVICES</span></h3></Link>
+      <Link to={'/location'}><h3>Our <span>LOCATION</span></h3> </Link>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderDesktop;
---
+++ .history/src/pages/Home/HomeDesktop_20211020203940.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable no-octal-escape */
+import React from "react";
+import styles from "./HomeDesktop.module.css";
+import Header from "../../components/Header/HeaderDesktop";
+import ArrowUp from "../../components/svg/arrowUp";
+import { HashLink } from "react-router-hash-link";
+import "react-scroll";
+import Footer from "../../components/Footer/Footer";
+import Arrow from "../../components/svg/arrow";
+
+function HomeDesktop(this: any): JSX.Element {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <Header />
+      </div>
+      <main
+        className={styles.main}
+        style={{
+          // eslint-disable-next-line no-useless-concat
+          backgroundImage: "url(" + "/assets/6.webp" + ")",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center right",
+          width: "100vw",
+        }}
+      >
+        <section id={"section1"} className={styles.section}>
+          <div className={styles.pWrapper}>
+            <h2 className={styles.subheader}>Integrity</h2>
+            <p>
+              Our mission is to provide efficient and professional legal
+              services tailored to meet the needs of clients, domestically,
+              regionally and internationally.
+            </p> 
+            <HashLink to={"/#section2"} className={styles.links} smooth={true}>
+            <Arrow />
+          </HashLink>
+          </div>{" "}
+         
+        </section>
+
+        <section id={"section2"} className={styles.section}>
+          <div className={styles.pWrapper}>
+            <h2 className={styles.subheader}>Professionalism</h2>
+            <p>
+              Wangui Gathere Advocates is a is a medium sized legal entity that
+              provides high level legal services in banking and financial law,
+              public finance and accounting law, commercial and corporate law,
+              insurance law, Conveyance and property law, succession law, civil
+              and criminal litigation and alternative dispute resolution. At
+              Wangui Gathere we aim to provide comprehensive legal services
+              geared at achieving our clients’ business, personal and
+              professional goals.
+            </p> 
+            <HashLink to={"/#section5"} className={styles.links} smooth={true}>
+            <Arrow />
+          </HashLink>
+          </div>
+          
+        </section>
+
+        <section id={"section3"} className={styles.section}>
+          <div className={styles.pWrapper}>
+            <h2 className={styles.subheader}>Efficency</h2>
+            <p>
+              Our vision is to fuse the law and the future, to provide our
+              clients with skilled legal advice in a timely and efficient
+              manner. We strive to handle each matter with accountability and
+              integrity. We focus our attention on the legal aspects of our
+              client’s business so that our clients can focus their attention on
+              the success of their business. Our vision reflects our values:
+              integrity, service, excellence and teamwork.
+            </p> <div className={styles.arrow}>
+            <HashLink to={"/#section1"} className={styles.links} smooth={true}>
+            <ArrowUp />
+          </HashLink></div>
+          </div>
+          
+        </section>
+
+        <section id="section5" className={styles.footer}>
+          <Footer />
+        </section>
+      </main>
+    </div>
+  );
+}
+export default HomeDesktop;
